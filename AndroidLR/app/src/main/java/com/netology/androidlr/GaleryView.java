d6b69c769c2ee76d8cfa40bf9aaa10233a08d5fb
Sitaara <perfectzvekla@gmail.com>
2020-07-07 14:36:27 +0300

    Initial commit

---
+++ AndroidLR/app/src/main/java/com/netology/androidlr/GaleryView.java
@@ -0,0 +1,14 @@
+package com.netology.androidlr;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GaleryView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_galery_view);
+    }
+}
