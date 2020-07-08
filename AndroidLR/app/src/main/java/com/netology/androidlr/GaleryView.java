72e8620cb05e5b3bc7f042edfe22b6e4962447f3
Sitaara <perfectzvekla@gmail.com>
2020-07-08 18:12:23 +0300

    обновление репозитория

--- AndroidLR/app/src/main/java/com/netology/androidlr/GaleryView.java
+++ AndroidLR/app/src/main/java/com/netology/androidlr/GaleryView.java
@@ -2,7 +2,13 @@ package com.netology.androidlr;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class GaleryView extends AppCompatActivity {
 
@@ -10,5 +16,36 @@ public class GaleryView extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_galery_view);
+
+        TextView vwResult = (TextView) findViewById(R.id.vwResult);
+        String rand = String.valueOf(Math.round( Math.random() *100));
+       // vwResult.setText(R.string.img_link);
+        vwResult.setText (getText( R.string.img_link) + rand);
+
+        Button btnNext = (Button) findViewById(R.id.btnNext);
+        Button btnPrev = (Button) findViewById(R.id.btnPrevious);
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                nextButtonClick();
+            }
+        });
+
+        btnPrev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                prevButtonClick();
+            }
+        });
+    }
+
+    private  void nextButtonClick(){
+        Intent intent = new Intent(getApplicationContext(), GaleryView.class);
+        startActivity(intent); //запуск
+    }
+
+    private void prevButtonClick(){
+        finish();
     }
 }
